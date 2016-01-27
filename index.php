9de374ff63e4db96d53ab75a706497a1f999c1de
STigos <sharktigo@gmail.com>
2016-01-27 16:17:53 +0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Кейк плюс изготовление тортов на заказ в г. Миассе</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="dist/css/bootstrap.css">
+    <link rel="stylesheet" href="dist/css/myStyle.css">
+    <script src="dist/js/jquery-2.1.4.js"></script>
+    <script src="dist/js/bootstrap.js"></script>
+    <script src="dist/js/myscript.js"></script>
+</head>
+<body class="">
+    <div class="nav navbar-light bg-inverse ">
+        <button class="navbar-toggler pull-right" data-toggle="collapse" data-target="#collapsingNav">
+            &#9776;
+        </button>
+        <a class="navbar-brand" type="button" href="index.php">Кейк плюс</a>
+        </div>
+<div class="collapse bg-inverse p-a-1" id="collapsingNav">
+    <h3 >контент</h3>
+</div>
+
+<div class="container">
+<div >
+    <h1 class="text-lg-center text-md-center">Заголовок страницы</h1>
+</div>
+    <hr>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+           ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+           laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+           voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+           non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+
+<div class="row bg-faded">
+    <div class="col-lg-12 col-md-12">
+    <p class="text-info">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+        officia deserunt mollit anim id est laborum.
+    </p>
+    </div>
+</div>
+    <div class="col-lg-12"></div>
+    <div class="col-lg-6 col-md-6 col-sm-12 bg-info ">
+        <p >
+            Нажмите на кнопку  "Модальное" что бы увидеть модальное окно.
+        </p>
+        <button class="btn btn-secondary-outline" type="button" data-toggle="modal" data-target="#MyModal">Модальное</button>
+    </div><div class="col-lg-6 col-md-6 col-sm-12 ">
+        <p >
+            Нажмите на кнопку  "Измениь" что бы увидеть результат.
+        </p>
+        <button class="btn btn-secondary-outline" type="button" onclick="setColor()">Изменить</button>
+    </div>
+    <div class="modal fade" id="MyModal">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+                <h4 class="modal-title">Заголовок модального окна</h4>
+              </div>
+              <div class="modal-body">
+                <p>Контент модального окна &hellip;</p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Отменить</button>
+                <button type="button" class="btn btn-primary">Сохранить</button>
+              </div>
+            </div><!-- /.modal-content -->
+          </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
+</div>
+<script>
+    $('.navbar-toggler').click(function(event){
+       this.css('color','red');
+    })
+</script>
+</body>
