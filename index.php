5fc0c0e113c0935cd4c40a19acb01db795e7d712
STigos <sharktigo@gmail.com>
2016-02-01 14:56:52 +0500

    **********

--- index.php
+++ index.php
@@ -31,31 +31,36 @@
     </div>
 </nav>
 
-<div class="container">
-<div >
-    <h1 class="text-lg-center text-md-center">Заголовок страницы</h1>
-</div>
-    <hr>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
-           ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-           laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-           voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-           non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-12  bg-faded">
 
-<div class="row bg-faded">
-    <div class="col-lg-12 col-md-12">
-    <p class="text-info">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
-        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-        officia deserunt mollit anim id est laborum.
-    </p>
+        <div class="p-a-3" >
+            <h1 class="text-lg-center text-md-center">Заголовок страницы</h1>
+        </div>
+            <hr>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+                   ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                   laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                   voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                   non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+
+
+            <div class="col-lg-12 col-md-12">
+            <p class="text-info">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                officia deserunt mollit anim id est laborum.
+            </p>
+             </div>
+        </div>
     </div>
-</div>
-    <div class="col-lg-12"></div>
+<div class="col-lg-12 bg-inverse">
+
     <div class="col-lg-6 col-md-6 col-sm-12 bg-info ">
         <p >
             Нажмите на кнопку  "Модальное" что бы увидеть модальное окно.
@@ -86,6 +91,8 @@
             </div><!-- /.modal-content -->
           </div><!-- /.modal-dialog -->
     </div><!-- /.modal -->
+</div>
+</div>
 </div>
 <script>
     $('.navbar-toggler').click($.css('color','red');
