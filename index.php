becb7012de3aed2824153f17a979afdad823dbc5
Tigran Sarkisyan <sharktigo@gmail.com>
2016-02-17 21:55:46 +0500

    Пара файлов на HTML5

--- index.php
+++ index.php
@@ -8,13 +8,12 @@
     <title>Кейк плюс изготовление тортов на заказ в г. Миассе</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="dist/css/bootstrap.css">
-    <link rel="stylesheet" href="dist/css/myStyle.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
     <script src="dist/js/bootstrap.js"></script>
     <script src="dist/js/myscript.js"></script>
 </head>
-<body class="">
-<nav class="navbar navbar-full bg-inverse">
+<body>
+<nav class="navbar navbar-full navbar-dark bg-inverse">
     <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar2">
         &#9776;
     </button>
@@ -23,7 +22,7 @@
         <a class="navbar-brand" href="#">Кейк плюс</a>
         <ul class="nav  navbar-nav ">
             <li class="nav-item">
-                <a class="nav-link" href="#">Торты на заказ</a>
+                <a class="nav-link" href="test.html" >AwesomeCo Blog</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">Контакты</a>
@@ -32,96 +31,77 @@
     </div>
 </nav>
 
+
 <div class="container-fluid bg-faded">
+    <div class="row">
+        <div class="col-lg-12">
 
-    <header>
-        <div >
-        <h1 class="text-lg-center text-md-center p-a-2">Заголовок страницы</h1>
+                        <div class=" text-lg-center text-md-center p-a-2" >
+                            <h1>&#171;Кейк плюс&#187;</h1>
+                            <h3> Изготовление тортов на заказ</h3>
+                        </div><hr>
+                      <div class="col-lg-12 col-md-12 col-sm-12">
+                          <div class="col-lg-6 col-md-6 col-sm-12">
+                              <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+                               ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                               laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                               voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                               non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                              </p>
+                          </div>
+                          <div class="col-lg-6 col-md-6">
+                              <table class="table">
+                                  <tbody>
+                                    <tr >
+                                        <td>Колонка 1 </td>
+                                        <td>Колонка 2</td>
+                                        <td>Колонка 3</td>
+                                    </tr>
+                                  <tr>
+                                      <td>.....</td>
+                                      <td>.....</td>
+                                      <td>.....</td>
+                                  </tr><tr>
+                                      <td>.....</td>
+                                      <td>.....</td>
+                                      <td>.....</td>
+                                  </tr><tr>
+                                      <td>.....</td>
+                                      <td>.....</td>
+                                      <td>.....</td>
+                                  </tr>
+                                  </tbody>
+                              </table>
+                          </div>
+                      </div>
+                    <div class="col-lg-12 col-md-12">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                            et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                            dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                            officia deserunt mollit anim id est laborum.
+                    </div>
         </div>
-    </header>
-    <hr>
+    <div class="col-lg-12 bg-inverse ">
+        <footer >
         <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
-           ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-           laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-           voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-           non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            <cite>Tigos&copy;</cite>
         </p>
+            <address>Изготовлено при помощи <a href="http://v4-alpha.getbootstrap.com">Bootstrap </a> </address>
+        </footer>
 
-<div class="row bg-faded">
-    <div class="col-lg-12 col-md-12">
-    <p class="text-info">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
-        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-        officia deserunt mollit anim id est laborum.
-    </p>
+
+
     </div>
-</div>
-    <div class="col-lg-12"></div>
-    <div class="col-lg-6 col-md-6 col-sm-12 bg-info ">
-        <p >
-            Нажмите на кнопку  "Модальное" что бы увидеть модальное окно.
-        </p>
-        <button class="btn btn-secondary-outline" type="button" data-toggle="modal" data-target="#MyModal">Модальное</button>
-    </div><div class="col-lg-6 col-md-6 col-sm-12 ">
-        <p >
-            Нажмите на кнопку  "Измениь" что бы увидеть результат.
-        </p>
-        <button class="btn btn-block" type="button" onclick="setColor()">Изменить</button>
     </div>
-    <div class="modal fade" id="MyModal">
-          <div class="modal-dialog" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-                <h4 class="modal-title">Заголовок модального окна</h4>
-              </div>
-              <div class="modal-body">
-                <p>Контент модального окна &hellip;</p>
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Отменить</button>
-                <button type="button" class="btn btn-primary">Сохранить</button>
-              </div>
-            </div><!-- /.modal-content -->
-          </div><!-- /.modal-dialog -->
-    </div><!-- /.modal -->
+
 </div>
-<div class="col-lg-12 bg-inverse">
-    <div class="col-lg-4 col-lg-offset-4 p-t-1">
-        <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
-        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-        officia deserunt mollit anim id est laborum.
-         </p>
-        <button type="button" class="btn btn-primary" onclick="opac()" aria-hidden="true">Push</button>
-    </div>
-</div>
-<div class="col-lg-12 col-md-12 col-sm-12 bg-faded">
-    <form  class="" action="phpaction.php" name="test_form" method="post">
-        <div class="input-group">
-            <input  type="email" name="adreses" id="mailID" >
-            <input type="button" class="btn btn-primary" value="Отправить"><span class="input-group-btn"></span>
-            <label for="mailID">Введите адресс</label>
-        </div>
-    </form>
-</div>
+<?php
+
+?>
+
 <script>
-    var a =10;
-    var myFunc = function(){
-        var b = 20;
-        var c = 8;
-        return function (){
-            return((a+b)/c);
-        };
-    };
-var anoverFunc = myFunc();
-    console.log(anoverFunc());
+
 </script>
 </body>
